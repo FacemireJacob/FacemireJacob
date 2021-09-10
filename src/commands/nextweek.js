1deaeaf6b6b939bdad3ae82245d30508021155fa
FacemireJacob <71603741+FacemireJacob@users.noreply.github.com>
2021-09-10 18:05:34 -0400

    Delete src directory

--- src/commands/nextweek.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { upcomingAssignments } = require('./templates/assignments.js');
-
-exports.name = ['nextweek', 'nw'];
-exports.call = upcomingAssignments(1);
-exports.help = 'Output assignments due next week\n`!nextweek [filter]` e.g. `!nextweek 105`';
